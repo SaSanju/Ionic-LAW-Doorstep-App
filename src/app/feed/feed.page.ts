b4615e80d9357c9e8569054bf799932b53499285
SaSanju <sayakbhattacharyyya@gmail.com>
2022-02-18 20:16:58 +0530

    Dummy Feed screen

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+import { FeedService } from '../services/feed.service';
 
 @Component({
   selector: 'app-feed',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
+  public projects: [];
 
-  constructor() { }
+  constructor(private toastCtrl: ToastController, private _feedService: FeedService) { }
 
   ngOnInit() {
+    this.getProjects();
   }
+
+  public getProjects() {
+		this._feedService.getFeeds().subscribe(async res => {
+			if (res) {
+				this.projects = res;
+				console.log(this.projects);
+			} else {
+				let toast = await this.toastCtrl.create({
+					message: res.message,
+					duration: 2500
+				});
+				await toast.present();
+			}
+		});
+	}
 
 }
---
+++ src/app/services/feed.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedService {
+  // endpoint: any = environment.endpoint;
+  endpoint: string = "https://baconipsum.com/api/?type=meat-and-filler";
+	public token;
+	public headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+	constructor(private _http: HttpClient) {
+		// this.token = _userService.getToken();
+	}
+
+	private extractData(res: Response) {
+		return res || [] || {};
+	}
+
+	getFeeds(): Observable<any> {
+		// let headers = this.headers.set('Authorization', token);
+		return this._http.get(this.endpoint).pipe(map(this.extractData));
+	}
+
+	// public addProject(token, project: Project): Observable<any> {
+	// 	let headers = this.headers.set('Authorization', token);
+	// 	let params = JSON.stringify(project);
+	// 	return this._http.post(this.endpoint + 'projects/', params, { headers: headers }).pipe(map(this.extractData));
+	// }
+
+	// public editProject(project: Project, token): Observable<any> {
+	// 	let params = JSON.stringify(project);
+	// 	let headers = this.headers.set('Authorization', token);
+	// 	return this._http.put(this.endpoint + 'projects/' + project._id, params, { headers: headers }).pipe(map(this.extractData));
+	// }
+
+	// public deleteProject(id, token): Observable<any> {
+	// 	let headers = this.headers.set('Authorization', token);
+	// 	return this._http.delete(this.endpoint + 'projects/' + id, { headers: headers }).pipe(map(this.extractData));
+	// }
+}
