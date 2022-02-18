3329d4511ec761525af79b56e744c33c4ef4a211
SaSanju <sayakbhattacharyyya@gmail.com>
2022-02-18 19:12:17 +0530

    Basic Layout

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,40 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: 'feed',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../feed/feed.module').then( m => m.FeedPageModule)
+          }
+        ]
+      },
+      {
+        path: 'gallery',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../gallery/gallery.module').then( m => m.GalleryPageModule)
+          }
+        ]
+      },
+      {
+        path: 'create',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../create/create.module').then( m => m.CreatePageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/home/feed',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
